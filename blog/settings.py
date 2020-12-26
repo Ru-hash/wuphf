87a5b295024047d3cf00c1ab566462ec29935dfa
Ruthra-Ramanan <rurab126@gmail.com>
2020-12-26 15:27:55 +0530

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rogj%k-9k^w)&9s*rjr%79&+2#9p@d*h6d_!z%=p^6u&zxl#y)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
